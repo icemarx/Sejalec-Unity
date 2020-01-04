d02f26b04070b215c223a55076b3cc86c3c856e1
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-01-04 13:37:14 +0100

    Implement camera rotation as third person camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
+
+    public float rotSpeed;
+    public Transform Target, Player;
+
+    float x;
+    float y;
+    public float camYMin;
+    public float camYMax;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
+    
+    void LateUpdate() {
+        x += Input.GetAxis("Mouse X") * rotSpeed;
+        y -= Input.GetAxis("Mouse Y") * rotSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        y = Mathf.Clamp(y, camYMin, camYMax);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(y, x, 0);
+        Player.rotation = Quaternion.Euler(0, x, 0);
     }
 }
