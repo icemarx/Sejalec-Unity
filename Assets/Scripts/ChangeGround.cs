57afe4376a0a19b5eb8a896334101f8d20bf9bf0
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-01-04 11:38:52 +0100

    Add terrain generation

---
+++ Assets/Scripts/ChangeGround.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGround : MonoBehaviour
+{
+    public Material[] material;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    public void ChangeMaterial(int index)
+    {
+        rend.sharedMaterial = material[index];
+    }
+}
---
+++ Assets/Scripts/VoxleFarm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxleFarm : MonoBehaviour
+{
+    public GameObject groundBlock;
+    public float amplitude = 5f;
+    public float freq = 8f;
+
+    void Start()
+    {
+        generateTerrain();
+    }
+
+    void generateTerrain()
+    {
+        int cols = 75;
+        int rows = 75;
+
+        for(int x = 0; x<cols; x++)
+        {
+            for(int z = 0; z<rows; z++)
+            {
+                float y = Mathf.PerlinNoise(x/freq, z/freq) * amplitude;
+
+                GameObject newBlock = GameObject.Instantiate(groundBlock);
+                newBlock.transform.position = new Vector3(x, y, z);
+            }
+        }
+    }
+}
