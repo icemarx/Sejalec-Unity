5001c72c1d8dbc02afc0c62a4d8bb683d94aef7d
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-01-04 13:19:00 +0100

    Implement player movement

--- Assets/Scripts/SejalecController.cs
+++ Assets/Scripts/SejalecController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SejalecController : MonoBehaviour
-{
+public class SejalecController : MonoBehaviour{
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,10 @@ public class SejalecController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(h, 0, v) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
     }
 }
