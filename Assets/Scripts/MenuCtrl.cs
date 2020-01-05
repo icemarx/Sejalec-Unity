36fb8d934dcae4b5f76ef7d9fa81b73ccc329fc4
sn3m <33692360+sn3m@users.noreply.github.com>
2020-01-05 16:42:44 +0100

    Finish main menu

---
+++ Assets/Scripts/MenuCtrl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuCtrl : MonoBehaviour
+{
+	public GameObject Controls;
+	public GameObject Credits;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void ControlsOn()
+	{
+		Controls.SetActive(true);
+	}
+
+	public void ControlsOff()
+	{
+		Controls.SetActive(false);
+	}
+
+	public void CreditsOn()
+	{
+		Credits.SetActive(true);
+	}
+
+	public void CreditsOff()
+	{
+		Credits.SetActive(false);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
