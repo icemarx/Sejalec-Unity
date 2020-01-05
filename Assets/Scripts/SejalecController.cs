9a30040b1a7c0984bf17d034942f3222d58d3503
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-01-05 15:16:59 +0100

    Add visual display of the raycasting result when preparing to plant

--- Assets/Scripts/SejalecController.cs
+++ Assets/Scripts/SejalecController.cs
@@ -13,6 +13,9 @@ public class SejalecController : MonoBehaviour{
     private CharacterController controller;
     private Vector3 playerMovement;
 
+    private GameObject previously_selected;
+    private int previous_index = 0;
+
     // Start is called before the first frame update
     void Start() {
         controller = GetComponent<CharacterController>();
@@ -23,7 +26,8 @@ public class SejalecController : MonoBehaviour{
         Movement();
 
         // Plant when mouse clicked
-        if(Input.GetMouseButtonDown(0)) Plant();
+        if (Input.GetMouseButton(0)) SelectForPlanting();
+        if (Input.GetMouseButtonUp(0)) Plant();
     }
 
     void Movement() {
@@ -42,12 +46,7 @@ public class SejalecController : MonoBehaviour{
         controller.Move(playerMovement);
     }
 
-    void Plant() {
-
-        // compute position
-        Vector3 flower_position = transform.position + transform.forward;
-        flower_position.y = transform.position.y;
-        
+    void SelectForPlanting() {
         RaycastHit hit;
         Vector3 ray_start = transform.position + transform.forward;
         ray_start.y = transform.position.y + Vector3.up.y;
@@ -55,11 +54,49 @@ public class SejalecController : MonoBehaviour{
         // Debug.DrawLine(ray_start, ray_start + Vector3.down * 5f, Color.blue, 1000);
         if (Physics.Raycast(ray_start, Vector3.down, out hit, 5f)) {
             GameObject target = hit.collider.gameObject;
-            if(target.tag == "Dirt" || target.tag == "Seeded" || target.tag == "Grass") {
 
+            // check if the target is a new target and recolor the old
+            if (previously_selected != null && previously_selected != target) Deselect();
+            
+            // try to recolor the new target
+            if (target.tag == "Dirt")  Select(0, 1, target);
+            else if (target.tag == "Grass") Select(2, 1, target);
+            else if (previously_selected != null)
+                // new target is not a valid target
+                Deselect();
+
+        } else if (previously_selected != null) {
+            // no new target
+            Deselect();
+        }
+    }
+
+    void Select(int prev_index, int new_index, GameObject target) {
+        previous_index = prev_index;
+        previously_selected = target;
+        previously_selected.GetComponent<ChangeGround>().ChangeMaterial(new_index);
+    }
+
+    void Deselect() {
+        previously_selected.GetComponent<ChangeGround>().ChangeMaterial(previous_index);
+        previous_index = -1;
+        previously_selected = null;
+    }
+
+    void Plant() {
+        RaycastHit hit;
+        Vector3 ray_start = transform.position + transform.forward;
+        ray_start.y = transform.position.y + Vector3.up.y;
+
+        // Debug.DrawLine(ray_start, ray_start + Vector3.down * 5f, Color.blue, 1000);
+        if (Physics.Raycast(ray_start, Vector3.down, out hit, 5f)) {
+            GameObject target = hit.collider.gameObject;
+            if(target.tag == "Dirt" || target.tag == "Grass") {
                 Debug.Log("*planting noises*");
                 GameObject seed = Instantiate(Seed, hit.point, Quaternion.identity);
                 seed.transform.parent = target.transform;
+
+                Deselect();
             }
         }
     }
