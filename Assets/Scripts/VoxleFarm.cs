709eb92bdfa8443237bfae1433b50fdc7652fc69
Dominik Skapin <ds9825@student.uni-lj.si>
2020-01-07 11:44:15 +0100

    pseudo GenerateObjects()

--- Assets/Scripts/VoxleFarm.cs
+++ Assets/Scripts/VoxleFarm.cs
@@ -7,6 +7,9 @@ public class VoxleFarm : MonoBehaviour
 {
     public GameObject groundBlock;
     public GameObject borderBlock;
+	//public GameObject treeOfLife;
+	//public GameObject well;
+	//public GameObject kowzowlec;
 
     public float amplitude = 5f;
     public float freq = 8f;
@@ -53,7 +56,35 @@ public class VoxleFarm : MonoBehaviour
                 newBlock.transform.parent = transform;
             }
         }
+		
+		GenerateTree();
+		GenerateWell();
+		GenerateKowzowlec();
     }
+	
+	void GenerateTree() {
+		//izberi random block v notranjosti (x in z v obmocju 30-45)
+		//
+		//GameObject newTree = GameObject.Instantiate(treeOfLife);
+		//newTree.transform.position = new Vector3(randomBlock.position.x,randomBlock.position.y,randomBlock.position.z);
+		//newTree.transform.parent = transform; karkoli je ze to
+	}
+	
+	void GenerateWell() {
+		//izberi random block v zunanjem obrocu (x in z v obmocju 10-20 in 55-65)
+		//
+		//GameObject newWell = GameObject.Instantiate(well);
+		//newWell.transform.position = new Vector3(randomBlock.position.x,randomBlock.position.y,randomBlock.position.z);
+		//newWell.transform.parent = transform; karkoli je ze to
+	}
+	
+	void GenerateKowzowlec() {
+		//izberi block tik ob sejalcu na zacetku
+		//
+		//GameObject newKowzowlec = GameObject.Instantiate(kowzowlec);
+		//newKowzowlec.transform.position = new Vector3(block.position.x,block.position.y,block.position.z);
+		//newKowzowlec.transform.parent = transform; karkoli je ze to
+	}
 
     double BorderWeight(int x, int z)
     {
