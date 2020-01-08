d697d9b6f8988bd2a14a8b66e156879f84cc272e
sn3m <33692360+sn3m@users.noreply.github.com>
2020-01-08 23:30:22 +0100

    Add minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform sejalec;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = sejalec.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, sejalec.eulerAngles.y, 0f);
+    }
+}
