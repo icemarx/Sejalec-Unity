349a9c19d2b175c952d66fe2dde978ac8617455e
Dominik Skapin <ds9825@student.uni-lj.si>
2020-01-09 21:10:10 +0100

    nekineki

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -11,14 +11,32 @@ public class AIController : MonoBehaviour {
 	public int currentX;
 	public int currentZ;
 	
-	//public GameObject AIghost;
+	private bool goTime;
+	private bool isHidden;
 	
-	//izvede se naj na random casa med 30-60 sekund
-	//semen ne bo napadal ker jih je prevec lahko, razen ce jih omejimo
-	//if (rozica obstaja) do this
-	void GenerateAI() {
+	public GameObject duhec;
+	public GameObject thisObject;
+	
+	void Start() {
+		//startaj uro 
+		thisObject = GameObject.Instantiate(duhec);
+		thisObject.transform.position = new Vector3(35f, 10f, 35f);
+		//spawna na sredini in soundEffect roar or some shit
+		//thisObject vanish
+		isHidden = true;
+		//start timer
+	}
+	
+	void Update() {
+		if (!this) {
+			//premikaj se hahaha
+		}
+	}
+	
+	void EmergeAI() {
+		isHidden = false;
 		
-		//Random pozicija x in z v obmocju 10-65 na visini y = 10-15
+		//Random pozicija x in z v obmocju 29-54 na visini y = 10-15
 		
 		//najdi najblizjo rozico 
 		//izracunaj smer poti
@@ -27,6 +45,10 @@ public class AIController : MonoBehaviour {
 		//odstej tocke
 		//odzenes ga ce prides dovolj hitro do njega
 	
+	}
+	
+	void VanishAI() {
+		isHidden = true;
 	}
 	
 }
