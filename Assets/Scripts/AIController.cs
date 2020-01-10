68fcad62160065b824fd8b27f0c239aa6bc0ba17
Dominik Skapin <ds9825@student.uni-lj.si>
2020-01-10 02:13:40 +0100

    hoja se ne deluje prou

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour {
 
-    public float speed = 1f;
+    public float speed;
+	public float gravitySmoother;
+    public float flower_effect_radius;
 	
 	public GameObject Sejalec;
 	public GameObject duhecSkin;
 	public GameObject duhec;
-	public GameObject targetRoza;
+	public GameObject targetRoza = null;
 	
 	public GameObject gameManager;
 	
 	public AudioClip roar;
-    AudioSource audioSource;
+	public AudioClip sadNoise;
+    public AudioSource audioSource;
 	
 	void Start() {
-		Debug.Log("Start duhec");
+		//Debug.Log("Start duhec");
 		audioSource = GetComponent<AudioSource>();
 		
 		duhec = GameObject.Instantiate(duhecSkin);
@@ -26,7 +29,8 @@ public class AIController : MonoBehaviour {
 		
 		audioSource.PlayOneShot(roar, 1f);
 		
-		Invoke("vanishAI", 10);
+		//Invoke("vanishAI", 5);
+		Invoke("vanishAI", 10); //DEBUG MODE
 	}
 	
 	void Update() {
@@ -38,24 +42,39 @@ public class AIController : MonoBehaviour {
 		
 		// duhec blizu roze --> stop and suck its life!
 		if (isNearTargetRoza()) {
+			Invoke("deleteRoza", 5);
 		}
 		
 		// targetRoza obstaja --> duhec go towards it!
-		if (targetRoza) {
-			float step = speed * Time.deltaTime;
-			duhec.transform.position = Vector3.MoveTowards(duhec.transform.position, targetRoza.transform.position, step);
+		if (targetRoza != null) {
+			//float step = speed * Time.deltaTime;
+			//duhec.transform.position = Vector3.MoveTowards(duhec.transform.position, targetRoza.transform.position, step);
 			
+			float step = speed * Time.deltaTime;
+			
+			float tmp = duhec.transform.position.y;
+			duhec.transform.position = Vector3.MoveTowards(duhec.transform.position, targetRoza.transform.position, step);
+			duhec.transform.position = duhec.transform.position - Vector3.up * duhec.transform.position.y + Vector3.up * tmp;
 		}
 	}
 	
 	void emergeAI() {
-		Debug.Log("EMERGE duhec");
+		//Debug.Log("EMERGE duhec");
+		
+		List<GameObject> big_flowers = gameManager.GetComponent<GameManager>().getBigFlowers();
+		
+		// ni rozic ni duhca!
+		if (big_flowers.Count < 1) {
+			Invoke("emergeAI",5);
+			return;
+		}
 		
 		System.Random rand = new System.Random();
 		
 		float xDuhec = (float) rand.Next(29,55);
 		float zDuhec = (float) rand.Next(29,55);
-		float yDuhec = (float) rand.Next(10,16);
+		//float yDuhec = (float) rand.Next(10,16);
+		float yDuhec = 3f;
 		
 		duhec.transform.position = new Vector3(xDuhec, yDuhec, zDuhec);
 		duhec.SetActive(true);
@@ -66,32 +85,66 @@ public class AIController : MonoBehaviour {
 	}
 	
 	void vanishAI() {
-		Debug.Log("VANISH duhec");
+		//Debug.Log("VANISH duhec");
+		duhec.transform.position = new Vector3(-50f, -50f, -50f);
 		duhec.SetActive(false);
 		System.Random rand = new System.Random();
-		Invoke("emergeAI", rand.Next(30,61));
+		//Invoke("emergeAI", rand.Next(30,61));
+		Invoke("emergeAI", 5); //DEBUG MODE
 	}
 	
 	GameObject getClosest() {
 		List<GameObject> big_flowers = gameManager.GetComponent<GameManager>().getBigFlowers();
+		//Debug.Log("big_flowers: " + big_flowers.Count);
+		if (big_flowers.Capacity < 1) return null;
 		float minDistance = float.MaxValue;
 		int index = 0;
-		for (int i = 0; i < big_flowers.Capacity; i++) {
+		for (int i = 0; i < big_flowers.Count; i++) {
+			//Debug.Log("i: " + i);
 			float tempDistance = Vector3.Distance(duhec.transform.position, big_flowers[i].transform.position);
 			if (tempDistance < minDistance) {
+				//Debug.Log("nov index: " + i);
 				minDistance = tempDistance;
 				index = i;
 			}
 		}
+		//Debug.Log("index: " + index);
 		return big_flowers[index];
 	}
 	
 	bool isNearTargetRoza() {
-		return Vector3.Distance(duhec.transform.position, targetRoza.transform.position) < 1f;
+		if (targetRoza == null) return false;
+		else return Vector3.Distance(duhec.transform.position, targetRoza.transform.position) < 5f;
 	}
 	
 	bool isSejalecNearDuhec() {
 		return Vector3.Distance(Sejalec.transform.position, duhec.transform.position) < 5f;
+	}
+	
+	void deleteRoza() {
+		if(duhec.activeSelf && targetRoza != null){
+			GameObject.Destroy(targetRoza);
+			List<GameObject> list1 = gameManager.GetComponent<GameManager>().getBigFlowers();
+			list1.Remove(targetRoza);
+			gameManager.GetComponent<GameManager>().setBigFlowers(list1);
+			turnGrassInDirt();
+			targetRoza = null;
+			vanishAI();
+			audioSource.PlayOneShot(sadNoise, 1f);
+		}
+	}
+	
+	void turnGrassInDirt() {
+		Collider[] colliders = Physics.OverlapSphere(targetRoza.transform.position, flower_effect_radius);
+
+		foreach(Collider c in colliders) {
+			if(c.gameObject.tag == "Grass") {
+				c.gameObject.tag = "Dirt";
+				c.gameObject.GetComponent<ChangeGround>().ChangeMaterial(0);
+				
+				// changeCount v minus!!
+			}
+		}
 	}
 	
 }
